01aab1131cb601f89da2afb9fd75aa409a3a47f2
LockingReal <1191515088@qq.com>
2022-12-19 21:25:08 +0800

    refesh

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 //  github api
 //  octokit
-
-const octokit = new Octokit({ auth: `personal-access-token123` });
+const { Octokit } = require("octokit");
+const octokit = new Octokit({
+  auth: `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`,
+});
 
 // octokit api create issue
-
-await octokit.rest.issues.create({
-  owner: "octocat",
-  repo: "hello-world",
-  title: "Hello World from " + slug,
+octokit.rest.issues.create({
+  owner: "LockingReal",
+  repo: "issues-action",
+  title: "lockingreal first issue",
+  body:"test",
 });
